8f5de2195ac1b98044e0d4595f8abde49525750a
Sergey Karinskiy <sergey@karinskiy.ru>
2015-07-23 21:40:10 +0600

    Initial commit with parsing br, hr and quotes

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    $    = require('gulp-load-plugins')();
+
+gulp.task('server', function() {
+  gulp.src('.')
+    .pipe($.webserver({
+      livereload: true,
+      fallback: './demo/index.html',
+      directoryListing: true,
+      open: true
+    }));
+});
+
+gulp.task('build', function() {
+  gulp.src('./src/textToJson.js')
+    .pipe($.umd())
+    .pipe(gulp.dest('./lib'))
+    .pipe($.uglify())
+    .pipe($.rename({ suffix: 'min' }))
+    .pipe(gulp.dest('./lib'));
+});
+
+gulp.task('watch', ['build'], function(){
+  gulp.watch('./src/textToJson.js', ['build']);
+});
+
+gulp.task('default', ['server', 'watch']);
---
+++ src/textToJson.js
@@ -0,0 +1,44 @@
+'use strict';
+
+function TextToJson(text) {
+  var lines = text.split("\n"),
+      lines2 = [],
+      isEmptyLine = false;
+
+  lines.map(function(line, i) {
+    var lastInsert = lines2[lines2.length-1];
+
+    if (line==='') {
+      if (!isEmptyLine) {
+        lines2.push({ tag: 'br' });
+        isEmptyLine = true;
+      }
+      return;
+    }
+
+    isEmptyLine = false;
+
+    if (line==='...') {
+      lines2.push({ tag: 'hr' });
+      return;
+    }
+
+    if (line[0]==='>') {
+      line = line.substr(1);
+      if (typeof(lastInsert)==='object' && lastInsert.tag==='blockquote') {
+        lastInsert.value.push(line);
+      } else {
+        lines2.push({ tag: 'blockquote', value: [line] });
+      }
+      return;
+    }
+
+    lines2.push(line);
+
+  });
+
+  console.dir(lines2);
+
+  return lines2;
+
+}
